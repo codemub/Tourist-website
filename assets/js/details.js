f5d0ee374c9bf7390fa442638fc4b290dac07a89
Manojkumar <91625919+codemub@users.noreply.github.com>
2023-04-27 21:26:53 +0530

    Add files via upload

---
+++ assets/js/details.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper('.beast', {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    mousewheel: {
+      invert: false,
+    },
+    // autoHeight: true,
+    pagination: {
+      el: '.beast__pagination',
+      clickable: true,
+    }
+  });
+
+
+
+
